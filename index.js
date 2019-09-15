89f4a10720f7c2eca093698783d27b8eff3ee95e
Tracie Masek <42948851+traciemasek@users.noreply.github.com>
2019-09-15 14:54:54 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution here:
+function driversWithRevenueOver(drivers, revenue) {
+  return drivers.filter(function(driver) {
+    return driver.revenue > revenue;
+  });
+};
+
+function driverNamesWithRevenueOver(drivers, rev) {
+  let driversRevArr = driversWithRevenueOver(drivers, rev);
+  return driversRevArr.map(function(driver) {
+    return driver.name
+  });
+};
+
+function exactMatch(drivers, obj) {
+  return drivers.filter(function(driver) {
+    let k = Object.keys(obj)[0];
+    let v = Object.values(obj)[0];
+    return driver[k] === v;
+  });
+};
+
+function exactMatchToList(drivers, obj) {
+  let driversArr = exactMatch(drivers, obj);
+  return driversArr.map(function(driver) {
+    return driver.name
+  });
+};
+
